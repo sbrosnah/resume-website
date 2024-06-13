76576779767a353c16107260c677dfa9decae250
sbrosnah <77689031+sbrosnah@users.noreply.github.com>
2024-06-12 22:16:54 -0500

    d

--- chunk-5UOCDBRB.js
+++
@@ -1,1 +0,0 @@
-import{Ma as i,Na as a,Va as r,X as o,Ya as s}from"./chunk-YSNZGEDB.js";var m=(()=>{let e=class e{};e.\u0275fac=function(n){return new(n||e)},e.\u0275cmp=o({type:e,selectors:[["app-education"]],standalone:!0,features:[s],decls:2,vars:0,template:function(n,p){n&1&&(i(0,"p"),r(1,"education works!"),a())}});let t=e;return t})();export{m as a};
